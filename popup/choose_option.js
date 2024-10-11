df844b98c6a5e706cf991c380b2d8b90478e9bda
hrdl <31923882+hrdl-github@users.noreply.github.com>
2024-10-11 19:13:23 +0200

    Deduplicate tab logic

--- popup/choose_option.js
+++ popup/choose_option.js
@@ -4,37 +4,21 @@ for (const elem of document.querySelectorAll("[data-i18n]")) {
   elem.textContent = browser.i18n.getMessage(elem.attributes['data-i18n'].value);
 }
 
+queryWithCurrentTab = (tabToMsgFn) => {
+  var query = (typeof browser === "undefined") ? {active: true, windowId : browser.windows.WINDOW_ID_CURRENT}
+    :  {active: true, currentWindow: true};
+  browser.tabs.query(query, tabs => {
+        if (tabs.length > 0) {
+          browser.runtime.sendMessage(tabToMsgFn(tabs[0]));
+        }
+      });
+  window.close();
+};
+
 document.querySelector(".all").addEventListener("click", () => {
   browser.runtime.sendMessage({});
   window.close();
 });
-document.querySelector(".current").addEventListener("click", () => {
-  var query = (typeof browser === "undefined") ? {active: true, windowId : browser.windows.WINDOW_ID_CURRENT}
-    :  {active: true, currentWindow: true};
-  browser.tabs.query(query, tabs => {
-        if (tabs.length > 0) {
-          browser.runtime.sendMessage({url: tabs[0].url});
-        }
-      });
-  window.close();
-});
-document.querySelector(".container-all").addEventListener("click", () => {
-  var query = (typeof browser === "undefined") ? {active: true, windowId : browser.windows.WINDOW_ID_CURRENT}
-    :  {active: true, currentWindow: true};
-  browser.tabs.query(query, tabs => {
-        if (tabs.length > 0) {
-          browser.runtime.sendMessage({cookieStoreId: tabs[0].cookieStoreId});
-        }
-      });
-  window.close();
-});
-document.querySelector(".container-current").addEventListener("click", () => {
-  var query = (typeof browser === "undefined") ? {active: true, windowId : browser.windows.WINDOW_ID_CURRENT}
-    :  {active: true, currentWindow: true};
-  browser.tabs.query(query, tabs => {
-        if (tabs.length > 0) {
-          browser.runtime.sendMessage({url: tabs[0].url, cookieStoreId: tabs[0].cookieStoreId});
-        }
-      });
-  window.close();
-});
+document.querySelector(".current").addEventListener("click", () => queryWithCurrentTab((tab) => ({url: tab.url})));
+document.querySelector(".container-all").addEventListener("click", () => queryWithCurrentTab((tab) => ({cookieStoreId: tab.cookieStoreId})));
+document.querySelector(".container-current").addEventListener("click", () => queryWithCurrentTab((tab) => ({url: tab.url, cookieStoreId: tab.cookieStoreId})));
